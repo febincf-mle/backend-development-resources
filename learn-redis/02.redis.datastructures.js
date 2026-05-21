0963edf08f0e61b3803071c9ce564b23cbfc7840
febincf-mle <febin.web3dev@gmail.com>
2026-05-21 23:45:13 +0530

    learn: hashset and sorted sets

--- learn-redis/02.redis.datastructures.js
+++ learn-redis/02.redis.datastructures.js
@@ -25,11 +25,49 @@ const setDatastructureOps = async () => {
     console.log(saddResponse, sremResposne, smembersResponse, ismemberResponse);
 }
 
+// Sorted sets in redis.
+// methods: ZADD, ZRANGE, ZRANK, ZREM
+const sortedSetDatastructureOps = async () => {
+    const addResponse = await client.zAdd("leaderboard", [
+        {
+            score: 90,
+            value: "Febin",
+        },
+        {
+            score: 80,
+            value: "Jeslin",
+        }
+    ]);
+
+    const rangeResponse = await client.zRange("leaderboard", 0, -1);
+    const rankResponse = await client.zRank("leaderboard", "Febin");
+
+    console.log(addResponse, rangeResponse, rankResponse);
+}
+
+
+// Hashed set in redis
+// methods: HSET, HGET
+const hashedSetDatastructureOps = async () => {
+    const hsetAddResponse = await client.hSet("user:001", {
+        name: "febin",
+        age: 30,
+        gender: "male",
+    });
+
+    const hsetGetResponse = await client.hGet("user:001", "name");
+    const hsetGetAllResponse = await client.hGetAll("user:001");
+
+    console.log(hsetAddResponse, hsetGetResponse, hsetGetAllResponse);
+}
+
 async function main() {
     try {
         await client.connect();
         await arrayDatastructureOps();
         await setDatastructureOps();
+        await sortedSetDatastructureOps();
+        await hashedSetDatastructureOps();
         console.log("[INFO]: redis client connected");
     } catch (error) {
         console.log(error)
